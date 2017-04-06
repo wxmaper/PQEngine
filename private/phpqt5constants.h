ef714e5408a9988fb45b4363d2d1158baafb8721
wxmaper <wxmaper@gmail.com>
2017-04-06 22:44:26 +0700

    Add constants

---
+++ private/phpqt5constants.h
@@ -0,0 +1,17 @@
+#ifndef PHPQT5CONSTANTS_H
+#define PHPQT5CONSTANTS_H
+
+namespace PHPQt5NS {
+    const int EQUAL = 0;
+    const int NOT_EQUAL = 1;
+
+    const char QSTRING_CLASS[] = "QString";
+    const char QBYTEARRAY_CLASS[] = "QByteArray";
+    const char QUILOADER_CLASS[] = "QUiLoader";
+    const char QFILE_CLASS[] = "QFile";
+
+    const char PQ_UID[] = "__pq_uid";
+    const char PQ_ZHANDLE[] = "__pq_zhandle";
+}
+
+#endif // PHPQT5CONSTANTS_H
